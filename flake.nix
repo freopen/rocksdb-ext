c26a23146b36e8ec8c799194d7297cbad3d94860
Aleksei Zolotov <freopen@freopen.org>
2023-01-01 10:36:22 +0100

    Add RocksDB dep

--- flake.nix
+++ flake.nix
@@ -10,10 +10,8 @@
     devShells.x86_64-linux.default = let
       pkgs = nixpkgs.legacyPackages.x86_64-linux.extend fenix.overlays.default;
     in pkgs.mkShell {
-      nativeBuildInputs = with pkgs;
-        with fenix.packages.x86_64-linux; [
-          nil
-          nixfmt
+      nativeBuildInputs = (with pkgs; [ nil nixfmt clang ])
+        ++ (with fenix.packages.x86_64-linux; [
           (complete.withComponents [
             "cargo"
             "clippy"
@@ -22,7 +20,8 @@
             "rustfmt"
           ])
           rust-analyzer
-        ];
+        ]);
+      LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
     };
   };
 }
