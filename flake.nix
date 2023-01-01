37cfeef0b303e0fc02159763fa297405421807a2
Aleksei Zolotov <freopen@freopen.org>
2023-01-01 10:16:42 +0100

    Rust boilerplate

--- flake.nix
+++ flake.nix
@@ -1,8 +1,28 @@
 {
-  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; };
-  outputs = { self, nixpkgs }: {
-    devShells.x86_64-linux.default =
-      let pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      in pkgs.mkShell { nativeBuildInputs = with pkgs; [ nil nixfmt ]; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+  outputs = { self, nixpkgs, fenix }: {
+    devShells.x86_64-linux.default = let
+      pkgs = nixpkgs.legacyPackages.x86_64-linux.extend fenix.overlays.default;
+    in pkgs.mkShell {
+      nativeBuildInputs = with pkgs;
+        with fenix.packages.x86_64-linux; [
+          nil
+          nixfmt
+          (complete.withComponents [
+            "cargo"
+            "clippy"
+            "rust-src"
+            "rustc"
+            "rustfmt"
+          ])
+          rust-analyzer
+        ];
+    };
   };
 }
